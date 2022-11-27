f1f9676225601d614e28dd8585b4fdcc3a04e490
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2022-11-27 19:01:01 +0300

    Update Problem1to10.hs

--- Problem1to10.hs
+++ Problem1to10.hs
@@ -1,22 +1,24 @@
-
+--Problem1--
 myLast :: [a] -> a
 myLast = last
 
-
+--Problem2--
 myButLast :: [a] -> a
 myButLast xs = last (init xs)
 
-
+--Problem3--
 elementAt :: [a] -> Int -> a
 elementAt xs n = last (take n xs)
 
+--problem4--
 myLength :: [a] -> Int
 myLength = length
 
+--Problem5--
 myReverse :: [a] -> [a]
 myReverse = reverse
 
-
+--Problem6--
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = if xs == reverse xs then True else False
 
