3f142a7e503bb494819c86ee0df7cf07c1244142
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2022-11-30 16:43:46 +0300

    Update Problem1to10.hs

--- Problem1to10.hs
+++ Problem1to10.hs
@@ -22,4 +22,15 @@ myReverse = reverse
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = if xs == reverse xs then True else False
 
+--Problem7--
+
+data NestedList a = Elem a | List [NestedList a] 
+
+flatten :: NestedList a -> [a]
+flatten (Elem a   )   = [a]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List [])     = []
+
+--Problem8--
+
 
