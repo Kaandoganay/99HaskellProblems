89f0146e6b603630c45ecd382b7cd4b21a2238aa
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-02-03 00:50:17 +0100

    Update Problems11to20.hs

--- Problems11to20.hs
+++ Problems11to20.hs
@@ -42,7 +42,18 @@ split :: [a] -> Int -> ([a], [a])
 split xs n = splitAt n xs
 
 --problem18--
-
-slice xs n m  = drop k (take l (unwords xs)
+slice :: [a] -> Int -> Int -> [a]
+slice xs n m  = drop (k-1) (take l  xs)
     where l = max n m
           k = min n m
+
+--problem19--
+
+rotate :: [a] -> Int -> [a]
+rotate xs n | n > 0 = drop  n xs ++ take n xs
+            | n < 0 = drop (length xs + n) xs ++ take (length xs + n) xs
+
+--Problem20--
+
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n xs = (xs !!(n-1) , take (n-1) xs ++ drop n xs)
