0fab7ec6b16cb19d6dcf8b1c1929637889d05609
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-02-03 07:55:46 +0100

    Update Problems21to30.hs

--- Problems21to30.hs
+++ Problems21to30.hs
@@ -1,1 +1,12 @@
+module Problems21to30 where
 
+--Problem21--
+insert :: a -> [a] -> Int -> [a]
+insert a xs n = take (n-1) xs ++ a: drop n xs
+
+--Problem22--
+
+range :: (Num a, Enum a) => a -> a -> [a]
+range x y = [x,x+1..y]
+
+--Problem23--
