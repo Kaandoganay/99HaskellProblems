42d8d0e2a37dc94c893678350849e75ac3fc4d5e
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-02-08 12:55:46 +0100

    Update Problems31to41.hs

--- Problems31to41.hs
+++ Problems31to41.hs
@@ -1,1 +1,25 @@
 
+--Problem31--
+isprime :: Integer -> Bool
+isprime x = null [k | k<-[2..(x-1)], x `mod` k == 0]
+
+--Problem32--
+myGCD :: Integer -> Integer -> Integer
+myGCD x y
+      | y == 0     = abs x
+      | otherwise  = myGCD y (x `mod` y)
+
+--Problem33--
+
+coprime :: Integer -> Integer -> Bool
+coprime x y = myGCD x y == 1
+
+--Problem34--
+
+totient :: Integer -> Int
+totient x = length [k | k <- [1..(x-1)], coprime k x ]
+
+--Problem35--
+
+primefactors :: Integer -> [Integer]
+primefactors x = filter isprime [k | k<-[2..x], x `mod` k ==0 ]
