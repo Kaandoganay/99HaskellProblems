d62837c11e57d0797ba2b3196cf1aa00a50318ea
Kaandoganay <47761276+Kaandoganay@users.noreply.github.com>
2024-02-09 11:22:39 +0100

    Update Problems21to28.hs

--- Problems21to28.hs
+++ Problems21to28.hs
@@ -1,5 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
+{-# HLINT ignore "Eta reduce" #-}
 {-# HLINT ignore "Use camelCase" #-}
+import Data.List ( nub, subsequences )
 import System.Random
 
 --Problem21--
@@ -12,9 +15,37 @@ range :: (Num a, Enum a) => a -> a -> [a]
 range x y = [x,x+1..y]
 
 --Problem23--
-rndselect xs n = do
-    gen <- getStdGen
-    return $ take n [ xs !! x | x <- randomRs (0, length xs - 1) gen]
 
+rndselect :: Int -> [a] -> [a]
+rndselect n x = map (x!!) is
+ where is = take n . nub $ randomRs (0, length x - 1) (mkStdGen 100)
 --Problem24--
-diff_select n x= rndselect [1..x] n
+
+diffselect :: (Num a, Enum a) => Int -> a -> [a]
+diffselect x y = rndselect x [1..y]
+
+--Poblem25--
+rndPermu :: [a] -> [a]
+rndPermu xs = rndselect (length xs ) xs
+
+--Problem26--
+combinations :: Int -> [a] -> [[a]]
+combinations k ns = filter ((k==).length) (subsequences ns)
+
+--Problem27--
+combination :: Int -> [a] -> [([a],[a])]
+combination 0 xs     = [([],xs)]
+combination n []     = []
+combination n (x:xs) = ts ++ ds
+  where
+    ts = [ (x:ys,zs) | (ys,zs) <- combination (n-1) xs ]
+    ds = [ (ys,x:zs) | (ys,zs) <- combination  n    xs ]
+
+group :: [Int] -> [a] -> [[[a]]]
+group [] _ = [[]]
+group (n:ns) xs =
+    [ g:gs | (g,rs) <- combination n xs
+           ,  gs    <- group ns rs ]
+
+altogether :: [Int] -> [a] -> Int
+altogether (x:xs) xz = length (group (x:xs) xz)
